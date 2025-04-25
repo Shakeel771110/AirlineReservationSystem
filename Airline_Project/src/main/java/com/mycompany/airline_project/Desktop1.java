51db079bb01dcf58c2ad22f866ceb76905ba1d7d
Shakeel771110 <jamalishakeel763@gmail.com>
2025-04-25 00:13:18 -0700

    Add files via upload

---
+++ Airline_Project/src/main/java/com/mycompany/airline_project/Desktop1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.airline_project;
+
+import com.mycompany.airline.AddFlight;
+
+/**
+ *
+ * @author N TECH
+ */
+class Desktop1 {
+
+    static void add(AddFlight addFlight) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
